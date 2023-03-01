459c3a6da8f3c1678ba739352356be96fd359b4c
Esraa Mohamed Abodief <em4728644@gmail.com>
2023-03-02 01:36:49 +0200

    created services

--- js/main.js
+++ js/main.js
@@ -24,3 +24,67 @@ function linkAction() {
     navMenu.classList.remove("show-menu");
 }
 navLinks.forEach((link) => link.addEventListener("click", linkAction));
+
+// start skills
+const skillCategories = document.querySelectorAll(".skill__category");
+const skillsHeaders = document.querySelectorAll(".skill-category__header");
+function toggleSkills() {
+  let clickedCategory = this.parentNode;
+  if (clickedCategory.classList.contains("skill__open")) {
+    clickedCategory.classList.remove("skill__open");
+  } else {
+    clickedCategory.classList.add("skill__open");
+  }
+}
+
+skillsHeaders.forEach((skillHeader) => {
+  skillHeader.addEventListener("click", toggleSkills);
+});
+// end skills
+
+// start qualifications
+const tabs = document.querySelectorAll("[data-target]");
+const tabsContent = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabsContent.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
+// end qualifications
+// start services
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalCloseBtns = document.querySelectorAll(".services__modal-icon-close");
+
+const activateModal = (modalIndex) => {
+  modalViews[modalIndex].classList.add("modal__active");
+};
+
+const deactivateModal = (modalIndex) => {
+  modalViews[modalIndex].classList.remove("modal__active");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    activateModal(i);
+  });
+});
+
+modalCloseBtns.forEach((modalCloseBtns, i) => {
+  modalCloseBtns.addEventListener("click", () => {
+    deactivateModal(i);
+  });
+});
+
+// end services
